7c92e7162a18a55b06b8f70b918aa7c7877f90a3
Justin Gabotero <justin_gabotero@dlsu.edu.ph>
2026-02-18 14:02:01 +0800

    created initial file handling structure

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 
 typedef char String[32];
 typedef char StringLong[100];
+typedef char StringBuffer[255];
 
 typedef struct {
   int year;
@@ -13,10 +14,45 @@ typedef struct {
   int day;
 } Date;
 
+// TODO: separate user handling into another file.
 typedef struct {
   String user;
   StringLong password;
   Date creationDate;
 } User;
+
+int countFileLines(FILE *fp) {
+  int i, lineCount = 0;
+
+  if (fp == NULL) {
+    printf("Could not read file.\n");
+    return 1;
+  }
+
+  for (i = getc(fp); i != EOF; i = getc(fp)) {
+    if (i == '\n') {
+      lineCount++;
+    }
+  }
+
+  return lineCount;
+}
+
+int userList() {
+  StringBuffer buf;
+  FILE *userFile;
+  int i, lineCount = 0;
+
+  userFile = fopen("user.txt", "r");
+  lineCount = countFileLines(userFile);
+
+  fclose(userFile);
+
+  return 0;
+}
+
+int registerUser() {
+  return 0; // TODO: IMPL
+}
 
 int main(void) { return 0; }
