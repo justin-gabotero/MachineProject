3c716341e46096cedc080b4ce0d73bc1504234dc
Justin Gabotero <justin_gabotero@dlsu.edu.ph>
2026-02-18 13:45:02 +0800

    Initialize project

---
+++ main.c
@@ -0,0 +1,22 @@
+// Project Title: AnimoConnect
+// Author: Gabotero, Justin L.
+// CCPROG2 Machine Project
+
+#include <stdio.h>
+
+typedef char String[32];
+typedef char StringLong[100];
+
+typedef struct {
+  int year;
+  int month;
+  int day;
+} Date;
+
+typedef struct {
+  String user;
+  StringLong password;
+  Date creationDate;
+} User;
+
+int main(void) { return 0; }
