e634c71369fcf60d38576a772cba87b6e11554b0
nairuugp <gabpanganiban07@gmail.com>
2026-03-23 10:12:08 +0800

    Update donation_system.h

--- src/donation_system.h
+++ src/donation_system.h
@@ -1,2 +1,25 @@
+#ifndef DONATION_SYSTEM_H
+#define DONATION_SYSTEM_H
+
 #include <stdio.h>
 #include "user_system.h"
+
+typedef struct {
+char username [32]; //username of donor
+char foodType [32]; //type of food 
+int quantity; 
+int yearExpiration;
+int monthExpiration;
+int dayExpiration;
+int pickupLocation [128]
+} Donation;
+
+int addDonationPrompt(User donor);
+int createDonation(Donation in, Donation out);
+void writeDomation(Donation donation);
+void loadDonation(Donation list[], int maxCount);
+double computeDonationWasteReduction(Donation donation);
+double computeTotalWasteReduction(Donation list[], int count);
+void computeMonthlyStats(Donation list[], int count, int year, int month);
+
+#endif
