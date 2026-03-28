e3567589a867e9bf420fa3a264536cd57ed90aac
justin-gabotero <shinjozyro@gmail.com>
2026-03-28 21:43:17 +0800

    refactor donor struct to use User instead of a username string

--- src/donation_system.h
+++ src/donation_system.h
@@ -1,22 +1,22 @@
 #ifndef DONATION_SYSTEM_H
 #define DONATION_SYSTEM_H
 
-#include <stdio.h>
 #include "user_system.h"
+#include <stdio.h>
 
 typedef struct {
-char username [32]; //username of donor
-char foodType [32]; //type of food 
-int quantity; 
-int yearExpiration;
-int monthExpiration;
-int dayExpiration;
-int pickupLocation [128]
+  User donor;        // the donor user
+  char foodType[32]; // type of food
+  int quantity;
+  int yearExpiration;
+  int monthExpiration;
+  int dayExpiration;
+  char pickupLocation[128];
 } Donation;
 
-int addDonationPrompt(User donor);
-int createDonation(Donation in, Donation out);
-void writeDomation(Donation donation);
+int addDonationPrompt(User donor, Donation *outDonation);
+int createDonation(Donation in, Donation *out);
+void writeDonation(Donation donation);
 void loadDonation(Donation list[], int maxCount);
 double computeDonationWasteReduction(Donation donation);
 double computeTotalWasteReduction(Donation list[], int count);
